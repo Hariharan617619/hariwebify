9b8840d4e4cbced20305860249368d2c07e70092
Hariharan617619 <harry617619@gmail.com>
2025-02-25 15:21:05 +0530

    Render url change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/hariwebify/';
+$config['base_url'] = 'http://hariwebify.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
